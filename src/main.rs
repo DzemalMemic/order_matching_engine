f6cb4b932ec7e41d16afd498f3fcb1da5d440b19
Dzemal <d.memic@centroidsol.com>
2024-02-17 19:02:02 +0100

    Resolve MR comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use {order_matching_engine::order::Order,uuid::Uuid, rust_decimal::Decimal, chrono::Utc};
-use {order_matching_engine::models::order_type::OrderType, order_matching_engine::models::side::Side};
+use {chrono::Utc, order_matching_engine::order::Order, rust_decimal::Decimal, uuid::Uuid};
+use {
+    order_matching_engine::models::order_type::OrderType, order_matching_engine::models::side::Side,
+};
 
-fn main()
-{
-    let order = Order{
-        order_id : Uuid::new_v4(),
-        order_type : OrderType::Limit,
-        volume : Decimal::new (100, 2),  // 100.00
-        price : Decimal::new (1500, 2),  // 15.00
-        create_time : Utc::now(),
-        symbol : String::from("AAPL"),
-        side : Side::Bid,
+fn main() {
+    // region: create order
+    let order = Order {
+        order_id: Uuid::new_v4(),
+        order_type: OrderType::Limit,
+        volume: Decimal::new(100, 2), // 100.00
+        price: Decimal::new(1500, 2), // 15.00
+        create_time: Utc::now(),
+        symbol: String::from("AAPL"),
+        side: Side::Bid,
     };
 
-    println !("Created order: {:?}", order);
+    println!("Created order: {:?}", order);
+    // endregion: create order
 }
-
--- tests/order_tests.rs
+++ tests/order_tests.rs
@@ -3,7 +3,7 @@ use order_matching_engine::order::Order;
 use rust_decimal::Decimal;
 
 #[test]
-fn create_order_test1() {
+fn test_create_order_with_type_limit(){
     let order = Order::new(
         OrderType::Limit,
         Decimal::new(10, 2),
@@ -19,17 +19,17 @@ fn create_order_test1() {
 }
 
 #[test]
-fn create_order_test2() {
+fn test_create_order_with_type_market(){
     let order = Order::new(
         OrderType::Market,
         Decimal::new(50, 2),
-        Decimal::new(70, 2),
+        Decimal::new(0, 2),
         "Symbol2".to_string(),
         Side::Ask,
     );
     assert_eq!(order.order_type, OrderType::Market);
     assert_eq!(order.volume, Decimal::new(50, 2));
-    assert_eq!(order.price, Decimal::new(70, 2));
+    assert_eq!(order.price, Decimal::new(0, 2));
     assert_eq!(order.symbol, "Symbol2");
     assert_eq!(order.side, Side::Ask);
 }
